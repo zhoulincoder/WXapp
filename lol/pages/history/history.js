c71d11d5bd283582bb6350b2f4392f070dee682d
HenleyZ <ZhouLin1620@163.com>
2019-04-19 08:18:08 +0800

    first lol

---
+++ lol/pages/history/history.js
@@ -0,0 +1,36 @@
+Page({
+  data:{
+    history: [
+      {
+        name: '爷',
+        winFail: '1/2/3',
+        time: '2019-3-1',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      },
+      {
+        name: '你爷',
+        winFail: '1/2/3',
+        time: '2019-3-1',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      },
+      {
+        name: '二大爷',
+        winFail: '1/2/3',
+        time: '2019-3-1',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      },
+      {
+        name: '你二爷',
+        winFail: '1/2/3',
+        time: '2019-3-1',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      },
+      {
+        name: '你大爷',
+        winFail: '1/2/3',
+        time: '2019-3-1',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      }
+    ]
+  }
+})
---
+++ lol/pages/legends/legends.js
@@ -0,0 +1,31 @@
+Page({
+  data:{
+    legends: [
+      {
+        name: '艾克案例',
+        title: '刺客',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      },
+      {
+        name: 'yasuo',
+        title: '风男',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      },
+      {
+        name: 'huonan',
+        title: '法师',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      },
+      {
+        name: 'nibaba',
+        title: 'zhanshi',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      },
+      {
+        name: '寒冰',
+        title: 'Adc',
+        src: 'https://cn.bing.com/th?id=OIP.oA6t4A2nCRHBMset94ikUQHaEX&pid=Api&rs=1&p=0'
+      }
+    ]
+  }
+})
