d73a1b6f7895f162be0868a9fc17d0c94d38313f
HenleyZ <ZhouLin1620@163.com>
2019-05-01 16:57:28 +0800

    time show

--- ikcamp/pages/index/index.js
+++ ikcamp/pages/index/index.js
@@ -35,9 +35,79 @@ let handle = {
         langs: config.appLang || 'en'
       }
     })
-    .then(res => {
-      console.log(res);
-    })
+      .then(res => {
+        console.log(res);
+        if (res && res.status === 0 && res.data && res.data.length) {
+          let articleData = res.data
+          // 格式化原始数据
+          let formatData = this.formatArticleDate(articleData)
+          console.log(formatData)
+          this.renderArticle(formatData);
+        }
+      })
+  },
+  //date
+  dateConvert(dateStr) {
+    if (!dateStr) {
+      return ''
+    }
+    let today = new Date(),
+      todayYear = today.getFullYear(),
+      todayMonth = ('0' + (today.getMonth() + 1)).slice(-2),
+      todayDay = ('0' + today.getDate()).slice(-2);
+    console.log(today);
+    let convertStr = '';
+    let originYear = + dateStr.slice(0, 4);
+    let todayFormat = `${todayYear}-${todayMonth}-${todayDay}`
+    if (dateStr === todayFormat) {
+      convertStr = '今日'
+    } else if (originYear < todayYear) {
+      let splitStr = dateStr.split('-')
+      convertStr = `${splitStr[0]}年${splitStr[1]}月${splitStr[2]}日`
+    } else {
+      // convertStr = dateStr.replace('-', '月') + '日'
+    }
+    return convertStr
+  },
+  formatArticleDate(data) {
+    let formatData = undefined
+    if (data && data.length) {
+      formatData = data.map((group) => {
+        group.formateDate = this.dateConvert(group.date)
+        // 在group上加一个属性formateData 
+        if (group && group.articles) {
+          let formatArticleItems = group.articles.map((item) => {
+            item.hasVisited = this.isVisited(item.contentId)
+            return item
+          }) || []
+          group.articles = formatArticleItems
+        }
+        return group
+      })
+    }
+    return formatData
+  },
+  isVisited(contentId) {
+    let vistedArticles = app.globalData && app.globalData.vistedArticles || ''
+    return vistedArticles.indexOf(`${contentId}`) > -1
+  },
+  renderArticle(data) {
+    if(data && data.length){
+      let newList = this.data.articleList.concat(data)
+      this.setData({
+        articleList: newList,
+        hiddenLoading: true
+      })
+    }
+  },
+  onReachBottom() {
+    if(this.data.hasMore) {
+      let nextPage = this.data.page + 1;
+      this.setData({
+        page: nextPage
+      })
+      this.requestArticle()
+    }
   }
 }
 Page(handle);
